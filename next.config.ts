3c70a4eacad974fbec3407136918ed7f9b85596c
jenniferguayta@gmail.com <jenniferguayta@gmail.com>
2025-01-18 17:11:36 -0500

    cambio para imagenes

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // Habilita la exportación estática
   trailingSlash: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'photos.app.goo.gl',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
